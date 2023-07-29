b9b351b4dffb00cc9d9ddbcb7045ff90e7d3f0a6
mik-dmi <122227240+mik-dmi@users.noreply.github.com>
2023-07-30 00:17:59 +0100

    The calculator project is finished.

--- java_script.js
+++ java_script.js
@@ -17,8 +17,8 @@ function setButtons(){
             if(i === 3 ) aux = 1;
         }     
     } 
-    gridNumb.style.gridTemplateColumns = 'repeat(4, 108px)';
-    gridNumb.style.gridTemplateRows = 'repeat(4, 110px)';
+    gridNumb.style.gridTemplateColumns = 'repeat(4, 98px)';
+    gridNumb.style.gridTemplateRows = 'repeat(4, 95px)';
     /*adding the text content to the buttons*/
     const addButton = document.getElementById('units4');
     addButton.textContent = '+';
@@ -28,48 +28,16 @@ function setButtons(){
     divButton.textContent = '/';
     const mulButton = document.getElementById('units16');
     mulButton.textContent = 'x';
-    const decimalButton = document.getElementById('units15');
+    const decimalButton = document.getElementById('units13');
     decimalButton.textContent = '.';
     const zeroButton = document.getElementById('units14');
     zeroButton.textContent = '0';
-    const clearButton = document.getElementById('units13');
-    clearButton.textContent = 'C';    
+    const equalButton = document.getElementById('units15');
+    equalButton.textContent = '='; 
+    equalButton.style.backgroundColor = "rgba(31, 31, 157, 0.848)";
 }
 /*---------------------------------------------------------------------------------*/
-
-
-const gridNumb = document.querySelector('.gridNumbers');
-setButtons();
-
-let firstInput = "0";
-let secondInput = "";
-let digit = "";
-let userInputs = [];
-let count = 0;
-let operationSign = "";
-
-const buttons = document.querySelectorAll('.buttons');
-
-buttons.forEach((selector)  => {
-    return selector.addEventListener('click', ()=>{      
-        if(parseFloat(selector.textContent)){
-            (operationSign != "") ? secondInput = secondInput + selector.textContent : firstInput = firstInput + selector.textContent;
-        }
-        if( selector.textContent == "+" || selector.textContent == "-" ||
-         selector.textContent == "/" || selector.textContent == "x"|| selector.textContent == "=" ){
-            if(secondInput != ""){
-                result = computeMath(firstInput,secondInput,operationSign);
-                console.log("1 input = "+ firstInput + " Signal "+ operationSign+ " Second input "+ secondInput+ " Result = " + result);
-                firstInput = result;
-                console.log(result)
-                secondInput = "";                
-            }
-        operationSign = selector.textContent;       
-        }
-        });  
-});
-
-
+/*--------------------------------- Math ----------------------------------------- */
 function computeMath(firstNumb,secondNumb,sign){
     switch(sign){
         case "+":
@@ -82,8 +50,78 @@ function computeMath(firstNumb,secondNumb,sign){
             return parseFloat(firstNumb) / parseFloat(secondNumb);
     }
 }
-   
+/* ---------------------------------------------------------------------------------*/ 
+
+const gridNumb = document.querySelector('.gridNumbers');
+const lowerDisplay = document.querySelector('.bottomDisplay');
+const topDisplay = document.querySelector('.topDisplay');
+const displayUp = document.querySelector('.topDisplay');
+setButtons();
+let firstInput = "0";
+let secondInput = "";
+let operationSign = "";
+
+
+const buttons = document.querySelectorAll('.buttons');
+
+buttons.forEach((selector)  => {
     
+    return selector.addEventListener('click', ()=>{      
+        if((parseFloat(selector.textContent) || selector.textContent == "0" || selector.textContent == "." ) && firstInput.length < 20 && secondInput.length < 20){
+            if(operationSign != ""){
+                secondInput = secondInput + selector.textContent;  
+                mainDisplay(lowerDisplay,secondInput);
+            }
+            else{
+                if(firstInput == 0) firstInput = ""; /*takes out the default 0*/
+                firstInput = firstInput + selector.textContent;
+                mainDisplay(lowerDisplay,firstInput);
+            }
+        
+        }
+        if(firstInput.length > 22 && secondInput.length > 22 || selector.textContent == "CLEAR"){
+            clearScreen();
+            
+        }
+        if(selector.textContent == "."){
+            document.getElementById("units13").disabled = true;/*reset the "." button*/
+        }
+        if(selector.textContent == "="){
+            result = computeMath(firstInput,secondInput,operationSign)
+            mainDisplay(lowerDisplay,result.toString());
+            displayUp.textContent = firstInput + operationSign +secondInput + selector.textContent ; 
+        } 
+        if( selector.textContent == "+" || selector.textContent == "-" ||
+         selector.textContent == "/" || selector.textContent == "x"){
+            if(secondInput != ""){    
+                result = computeMath(firstInput,secondInput,operationSign);
+                /*console.log("1 input = "+ firstInput + " Signal "+ operationSign+ " Second input "+ secondInput+ " Result = " + result);*/
+                firstInput = result.toString();
+                secondInput = "";  
+
+            }      
+        document.getElementById("units13").disabled = false;  /*reset the "." button*/
+        operationSign = selector.textContent;  
+        displayUp.textContent = firstInput + selector.textContent; 
+        mainDisplay(lowerDisplay,firstInput);    
+        }   
+    });  
+});
+
+
+function mainDisplay(display, currentInput){
+    display.textContent = currentInput ;
+}  
+function clearScreen(){
+    firstInput = "0";
+    secondInput = "";
+    operationSign = "";
+    displayUp.textContent = ""
+    mainDisplay(lowerDisplay,"0");
+
+} 
+
+
     
 
 
